7c990e82dbea3a7ee86d6a0875a6ce2fa67f15b4
Alexandracoder <acasaviajes@gmail.com>
2025-05-07 02:17:22 +0200

    AboutMe completed

--- AboutMe.java
+++ AboutMe.java
@@ -3,14 +3,21 @@ public class AboutMe {
         //inicializar una variable (del tipo que veas conveniente) para cada uno de los siguentes datos: nombre, edad, país, lenguaje de programación, horas, pasatiempo, lugar y reemplaza en el texto de impresión, la variable que corresponda.
 
         //Escribe tu código aquí
+        String name = "Alexandra";
+        int age = 43;
+        String country = "República Dominicana";
+        String programmingLanguage = "Java";
+        double hours = 15.5;
+        String hobby = "Cantar";
+        String place = " en todas partes";
 
         //Reemplaza <name> y <age> concatenando el String con las variables creadas. 
-        System.out.println("Mi nombre es <name>, tengo <age> años y soy de <country>.");
+        System.out.println("Mi nombre es " + name + ", tengo " + age + " años y soy de " + country +".");
 
         //Reemplaza <programmingLanguage> y <hours> concatenando el String con las variables creadas. 
-        System.out.println("Estoy estudiando <programmingLanguage> y practico <hours> horas al día.");
+        System.out.println("Estoy estudiando " + programmingLanguage + " y practico " + hours + " horas al día.");
 
         //Reemplaza <hobby> y <place> concatenando el String con las variables creadas.
-        System.out.println("Mi pasatiempo favorito es <hobby> y lo practico en <place>");
+        System.out.println("Mi pasatiempo favorito es "+ hobby + " y lo practico en" + place + ".");
     }
 }
